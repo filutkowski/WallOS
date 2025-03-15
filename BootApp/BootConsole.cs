42a6c3cbd32fa9cc1f0afeaf34dae9bc8f0d141a
Michał Filutkowski <filutkowski@gmail.com>
2025-03-15 07:57:15 +0100

    0.0 -- erroring -- not kernel

---
+++ BootApp/BootConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallOS.BootApp
+{
+    public static class BootConsole
+    {
+        public static void WriteInfo(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[INFO!]" + text);
+        }
+
+    }
+
+    
+}
---
+++ Kernel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.System.FileSystem.VFS;
+using Cosmos.System.FileSystem;
+using Cosmos.System.FileSystem.Listing;
+using Cosmos.System.FileSystem.FAT;
+using Cosmos.System.FileSystem.FAT.Listing;
+
+namespace WallOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static CosmosVFS fs;
+        public static string Path = @"0:\";
+        public static string DeafaultDosPath = @"0:\DOS";
+        public static string DeafaultSystemPath = @"0:\WallOS";
+        protected override void BeforeRun()
+        {
+            fs = new();
+            VFSManager.RegisterVFS(fs);
+            Console.Clear();
+            Console.SetWindowSize(90, 30); 
+            Console.OutputEncoding = Cosmos.System.ExtendedASCII.CosmosEncodingProvider.Instance.GetEncoding(437);
+            Console.WriteLine("WallOS booted successfully");
+        }
+
+        protected override void Run()
+        {
+        }
+    }
+}
